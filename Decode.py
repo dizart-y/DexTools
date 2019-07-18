cce20e500e134049c2206e078ea9e1f8dd7bbf30
DIckY CaNDRA <52904090+dizart-y@users.noreply.github.com>
2019-07-18 14:51:55 +0700

    Create Decode.py

---
+++ Decode.py
@@ -0,0 +1,165 @@
+#!/usr/bin/python2
+# DiZart-y
+# Author: DIcky CaNdRA
+# Email: budisulistiyorex@gmail.com
+# Support: Volta |PutraDicko|Edi|M.RAIS.S|MR.jihad212|Kusuma
+# Hargai Yang Buat Jangan Cuman Bisa Recode Kontol!! Recode? Mandul ^_^
+#*/kalau mau di hargai Copyright Yang Bikin
+import os
+import base64
+
+os.system("clear")
+
+print """     
+              DIZART 
+         Simple Dex Tools
+             DeCode V.3
+"""
+print "=================================="
+print "Author : DIcky CaNdRA              "
+print "Date   : 17-07-2019               "
+print "Support: Volta |PutraDicko|Edi|M.RAIS.S|MR.jihad212|Kusuma          "
+print "GitHub : https://github.com/dizart-y          "
+print "Member  : HackerCyber_Team               "
+print "========= PROGAMMING Is Pec ============"
+print "(01) Hammer"
+print "(02) Spammer-Grab"
+print "(03) Red Hawk"
+print "(04) Galau Tools"
+print "(05) Facebook MBF Pirmansx" 
+print "(06) Webdav" 
+print "(07) Metasploit" 
+print "(08) KaliNethunter" 
+print "(09) Infogals"
+print "(10) D-Tect"
+print "(0).ExitZ"
+xeit666h05t =raw_input("DiZart ~# ")
+
+if xeit666h05t == '01' or xeit666h05t == '1':
+
+            print("Install Hammer")
+            os.system("pkg update")
+            os.system("pkg upgrade")
+            os.system("pkg install python")
+            os.system("pkg install git")
+            os.system("git clone https://github.com/cyweb/hammer")
+            os.system("clear")
+            print("Done 0ce")
+
+if xeit666h05t == '02' or xeit666h05t == '2':
+
+            print("Install Spammer-Grab")
+            os.system("pkg update && pkg upgrade")
+            os.system("pkg upgrade")
+            os.system("pkg update")
+            os.system("pkg install git")
+            os.system("pkg install python")
+            os.system("git clone https://github.com/p4kl0nc4t/Spammer-Grab")
+            os.system("clear")
+            print("Done 0ce")
+
+if xeit666h05t == '03' or xeit666h05t == '3':
+
+            print("Install RedHawk Euyy")
+            os.system("apt update && apt upgrade")
+            os.system("apt install php")
+            os.system("apt install git")
+            os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK")
+            os.system("clear")
+            print("Done 0ce")
+
+if xeit666h05t == '04' or xeit666h05t == '4':
+
+            print("Install Galau Tools")
+            os.system("pkg update")
+            os.system("pkg install git")
+            os.system("pkg install python")
+            os.system("pkg install python2")
+            os.system("git clone https://github.com/DaffaTakarai/AutoGalauTools")
+            os.system("clear")
+            print("Done 0ce")
+
+if xeit666h05t == '05' or xeit666h05t == '5':
+
+            print("Install Facebook BruteForce")
+            os.system("pkg update && pkg upgrade")
+            os.system("pkg upgrade")
+            os.system("pkg update")
+            os.system("pkg install git")
+            os.system("apt install python2-dev")
+            os.system("pkg install python")
+            os.system("apt install wget")
+            os.system("pip2 install mechanize")
+            os.system("git clone https://github.com/DaffaTakarai/XEIT_Cyber")
+            os.system("clear")
+            print("Done 0ce")
+
+if xeit666h05t == '06' or xeit666h05t == '6':
+
+            print("Install Webdav")
+            os.system("apt update && apt upgrade") 
+            os.system("apt install python") 
+            os.system("apt-get install python-pip") 
+            os.system("pip2 install urllib3 chardet certifi idna requests") 
+            os.system("apt install openssl curl") 
+            os.system("pkg install libcurl") 
+            os.system("mkdir webdav") 
+            os.system("cd ~/webdav") 
+            os.system("wget https://pastebin.com/raw/HnVyQPtR -O webdav.py ")
+            os.system("chmod 777 webdav.py")
+            os.system("cd ~/")
+            os.system("clear")
+            print("Done 0ce")
+
+if xeit666h05t == '07' or xeit666h05t == '7':
+
+            print("Install Metasploit Euy")
+            os.system("apt update && apt upgrade")
+            os.system("apt install git")
+            os.system("apt install wget")
+            os.system("wget https://raw.githubusercontent.com/verluchie/termux-metasploit/master/install.sh")
+            os.system("chmod 777 install.sh")
+            os.system("sh install.sh")
+            os.system("clear")
+            print("Done 0ce")
+
+if xeit666h05t == '08' or xeit666h05t == '8':
+
+            print("Install KaliNethunter")
+            os.system("apt update && apt upgrade")
+            os.system("apt install git")
+            os.system("git clone https://github.com/Hax4us/Nethunter-In-Termux.git")
+            os.system("cd ~/Nethunter-In-Termux")
+            os.system("chmod 777 kalinethunter")
+            os.system("sh kalinethunter")
+            os.system("clear")
+            print("Done 0ce")
+
+if xeit666h05t == '9' or xeit666h05t == '9':
+
+            print("Install Infoga")
+            os.system("apt update && apt upgrade")
+            os.system("apt install git")
+            os.system("pkg install python2")
+            os.system("git clone https://github.com/m4ll0k/Infoga.git infoga")
+            os.system("cd infoga")
+            os.system("pip install -r req")
+            os.system("clear")
+            print("Done 0ce")
+
+if xeit666h05t == '10' or xeit666h05t == '10':
+
+            print("Install D-Tect")
+            os.system("apt update && apt upgrade")
+            os.system("apt-get install python")
+            os.system("apt-get install git")
+            os.system("git clone https://github.com/shawarkhanethicalhacker/D-TECT")
+            os.system("clear")
+            print("Done 0ce")
+
+if xeit666h05t == '99' or xeit666h05t == '99':
+
+            print("Mau Kemana?Buru Buru Amat")
+            print("Thanks All Mem HackerCyber_Team")
+            print(" Thnks To GhoztXSt229p --(Zart) =Dis62")
+            print("Byee:*")
